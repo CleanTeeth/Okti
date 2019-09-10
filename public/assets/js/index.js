b7d31b7d598a4036f2184b385a6c796bf3a0484b
DziadDawid <dawidfol@op.pl>
2019-09-10 18:27:58 +0200

    Refreshed forms

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,19 +12,24 @@ const registerForm = document.querySelector(".register__form");
 
 // Login form
 nav_items[0].addEventListener("click", () => {
-  console.log("ass");
   loginForm.classList.toggle('login__form--active');
+  
+  if($('.register__form--active').is(':visible')) {
+    registerForm.classList.toggle("register__form--active");
+  }
+
   nav.classList.toggle("navigation--active");
   menu.classList.toggle('hamburger--active');
 });
 
 // Register form
 nav_items[1].addEventListener("click", () => {
-  console.log("ass");
   registerForm.classList.toggle('register__form--active');
+  
+  if($('.login__form--active').is(':visible')) {
+    loginForm.classList.toggle("login__form--active");
+  }
+
   nav.classList.toggle("navigation--active");
   menu.classList.toggle('hamburger--active');
 });
-
-console.log(nav_items[0]);
-console.log(nav_items[1]);
